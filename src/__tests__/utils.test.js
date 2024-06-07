2d29fdf2da1d6985b53d28d28ed418d14f5ac7bd
Buddha Lo <lo_977@yahoo.com>
2024-06-06 23:22:50 -0700

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,22 @@
 // Your tests here
+
+// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true if the word is same when reverse ", () => {
+    expect(isPalindrome("madam")).toBe(true);
+  });
+  it("returns false if the word is not the same when reverse", () => {
+    expect(isPalindrome("love")).toBe(false);
+  });
+  it("return true for words that are a combination of uppercase and lowercase letters", () => {
+    expect(isPalindrome("RoTaTOr")).toBe(true);
+  });
+  it("throw an error if input has any non-alphabetic characters and non string input", () => {
+    expect(() => isPalindrome("say hi")).toThrow();
+    expect(() => isPalindrome("")).toThrow();
+    expect(() => isPalindrome("92352903")).toThrow();
+    expect(() => isPalindrome(".,@-=';#$%")).toThrow();
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+
+export function isPalindrome(word) {
+  if (!/^[A-Za-z]+$/.test(word)) {
+    throw new Error("Invalid input");
+  }
+  return word.toLowerCase() === word.split("").reverse().join("").toLowerCase();
+}
